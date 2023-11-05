6d9795e05a31e1cde24e5dd5f35b3bab91a4679b
Yury Kuzmich <yurakuzmich@gmail.com>
2023-11-05 21:53:27 +0500

    finished list

--- src/app/shop/list/list.component.ts
+++ src/app/shop/list/list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService, ShopItem } from 'src/app/API.service';
+import { ZenObservable } from 'zen-observable-ts';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.less']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+  public shopItemsList: Array<ShopItem> = [];
 
+  private subscription: ZenObservable.Subscription | null = null;
+
+  constructor(private api: APIService) { }
+
+  ngOnInit(): void {
+    this.api.ListShopItems().then((event) => {
+      this.shopItemsList = event.items as ShopItem[];
+      console.log(this.shopItemsList);
+    });
+
+    this.subscription = this.api.OnCreateShopItemListener().subscribe((event: any) => {
+      const newShopItem = event.value.data?.onCreateShopItem;
+
+      this.shopItemsList = [newShopItem, ...this.shopItemsList];
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+
+    this.subscription = null;
+  }
 }
