7f4c85b8be4d8b717efeb75ec351daf9a9f211f8
Yury Kuzmich <yurakuzmich@gmail.com>
2023-11-05 22:50:40 +0500

    finished auth

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app/app.module';
 import { Amplify } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
+// Auth.configure(aws_exports);
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,4 +1,7 @@
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+
 (window as any).global = window;
 (window as any).process = {
   env: { DEBUG: undefined },
 };
+(window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
